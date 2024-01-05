12cfc5f0f4e74d1673959bce8b366781c9aa2481
qingzi <tsy15632385483@163.com>
2024-01-05 14:47:47 +0800

    fix(router): 🧩 本地读取路由
    BREAKING CHANGE:  本地路由读取

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,11 +1,35 @@
 <template>
-  <div class="home card">
-    <img class="home-bg" src="@/assets/images/welcome.png" alt="welcome" />
+  <div class="home">
+    <div v-for="item in 12" :key="item.id" class="card" @click="handleCard">
+      {{ item }}
+    </div>
   </div>
 </template>
 
-<script setup lang="ts" name="home"></script>
+<script setup lang="ts">
+import { ref } from "vue";
+const projectarr = ref([
+  {
+    name: "桥梁项目",
+    image: "",
+    url: "http://webtest.cloudansys.com/bridge/#/Login"
+  }
+]);
+
+const handleCard = () => {
+  console.log(projectarr.value);
+};
+</script>
 
 <style scoped lang="scss">
-@import "./index.scss";
+.home {
+  display: flex;
+  flex-wrap: wrap;
+  height: 100%;
+  .card {
+    width: 24%;
+    height: 30%;
+    margin: 0 10px 10px 0;
+  }
+}
 </style>
