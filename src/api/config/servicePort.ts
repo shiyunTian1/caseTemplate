2a95f7cfb85b36d2f780c6d1c5b34db3d9b32bb9
qingzi <tsy15632385483@163.com>
2024-01-05 09:59:45 +0800

    base

---
+++ src/api/config/servicePort.ts
@@ -0,0 +1,11 @@
+/*
+ * @Author: qingzi tsy15632385483@163.com
+ * @Date: 2024-01-05 09:48:47
+ * @LastEditors: qingzi tsy15632385483@163.com
+ * @LastEditTime: 2024-01-05 09:49:37
+ * @FilePath: \Geeker-Admin\src\api\config\servicePort.ts
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+// 后端微服务模块前缀
+export const PORT1 = "/geeker";
+export const PORT2 = "/hooks";
