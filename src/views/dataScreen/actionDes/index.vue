e0c65df5bab4221ebbabca13ca4c8ab0ce3c7577
xiwang <14998518@qq.com>
2024-01-08 18:00:02 +0800

    perf: 优化页面结构

---
+++ src/views/dataScreen/actionDes/index.vue
@@ -0,0 +1,12 @@
+<!--
+  @description: 交互说明
+ -->
+<template>
+  <div class="home">交互说明</div>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+</script>
+
+<style scoped lang="scss"></style>
