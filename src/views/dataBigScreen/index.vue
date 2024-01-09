2209f81cb4ef5a969ad25811fcb6b04cf51a0895
孙宇 <1106894275@qq.com>
2024-01-09 11:33:52 +0800

    feat: 新建数据大屏首页

---
+++ src/views/dataBigScreen/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="card mb10">
+    <h4 class="title">数据大屏</h4>
+    <span class="text"> 数据大屏配置 </span>
+  </div>
+  <div class="card"></div>
+</template>
+
+<script setup lang="ts">
+import { ref, reactive } from "vue";
+</script>
+
+<style scoped lang="scss">
+.card {
+  .title {
+    margin: 0 0 15px;
+    font-size: 17px;
+    font-weight: bold;
+    color: var(--el-text-color-primary);
+  }
+  .text {
+    font-size: 15px;
+    line-height: 25px;
+    color: var(--el-text-color-regular);
+    .el-link {
+      font-size: 15px;
+    }
+  }
+}
+</style>
