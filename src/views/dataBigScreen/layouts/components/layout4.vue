cd9b3b5d90c9cbd65b0ab72554c816a618ce913c
xiwang <14998518@qq.com>
2024-01-17 21:53:20 +0800

    feat: 添加布局页面

---
+++ src/views/dataBigScreen/layouts/components/layout4.vue
@@ -0,0 +1,13 @@
+<template>
+  <el-container>
+    <el-aside width="200px">Aside</el-aside>
+    <el-container>
+      <el-header>Header</el-header>
+      <el-main>Main</el-main>
+    </el-container>
+  </el-container>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
